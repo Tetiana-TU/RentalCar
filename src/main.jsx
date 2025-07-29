25b7e8d98c1d7126bcbf39a74276700aeeec86ea
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-07-29 12:46:53 +0300

    car

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/RentalCar/" : "/",
+export default defineConfig({
   plugins: [react()],
-}));
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+  },
+});
