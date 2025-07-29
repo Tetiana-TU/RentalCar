c208135191870cb1e037641f915bea40d7704db0
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-07-29 11:19:33 +0300

    rental

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/RentalCar/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/RentalCar/" : "/",
   plugins: [react()],
-});
+}));
